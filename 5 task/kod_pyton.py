9647896ce2b9498875a9f2ce731dd483e77e5991
dexuxxx <81314321+dexuxxx@users.noreply.github.com>
2022-10-13 18:14:05 +0800

    Add files via upload

---
+++ 5 task/kod_pyton.py
@@ -0,0 +1,61 @@
+
+
+import serial
+import time
+lenghts = {'s':3, 'u':0,'d':0,'b':0}
+values = []
+
+def get_connection(port):
+    ser = serial.Serial(port,timeout=1)
+    return ser
+
+def send(ser,message, mesg_len):
+    ser.write(message)
+    time.sleep(0.005)
+    if mesg_len != 0:
+        data = ser.readline()
+        result = data.decode()
+        result = result.strip()
+        return (result)
+    
+    
+if __name__ == '__main__':
+    ser = get_connection("COM11")
+    while True:
+        print("Введите одну из команд: \nu - включить светодиод, \nd - выключить светодиод, \nauto - включение/выключение светодиода по показаниям датчика, \nrec - включить поточную передачу данных с датчика, \nb - регулировать яркость светодиода в зависимости от освещенности")      
+        inp = input()
+        if inp == "u" or inp == "d" :
+            send(ser, inp.encode(), lenghts[inp])
+        elif inp == "auto":
+            while True:
+                com = "s"
+                val = send(ser, com.encode(), lenghts[com])
+                if val:
+                    val = int(val)
+                    print(val)
+                    if val > 900:
+                        send(ser, 'd'.encode(), 0)
+                    else:
+                        send(ser, 'u'.encode(), 0)
+        elif inp == "rec":
+            while True: 
+                com = "s"
+                val = send(ser, com.encode(), lenghts[com])
+                if val:
+                    values.append(val)
+                print (values)
+        elif inp == "b":
+            while True: 
+                com = "s"
+                val = send(ser, com.encode(), lenghts[com])
+                if val:
+                    val = int(val)
+                    print(val)
+                    if val > 900:
+                        send(ser, 'b'.encode(), 0)
+                        brightness = send(ser, '50'.encode(),1)
+                    else:
+                        send(ser, 'b'.encode(), 0)
+                        brightness = send(ser, '255'.encode(),1)
+        else:
+            print("Неизваестная команда")
